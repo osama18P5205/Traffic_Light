8143422ffe2d5af44e8315974995f2807087edce
Sherif-Ahmed17 <66403092+Sherif-Ahmed17@users.noreply.github.com>
2021-11-02 20:04:24 +0200

    Update DIO.c

--- DIO.c
+++ DIO.c
@@ -6,36 +6,46 @@ void DIO.Init (int_8 port, int_8 pin, int_8 dir)
 
   set_bit(SYSCTL_RCGCGPIO_R, port)
   while((SYSCTL_PRGPIO_R & port) == 0){};
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;
-  DIO_WritePin(Pin,value);
   
     switch(port){
+        
       case port PORTA:
+        GPIO_PORTA_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTA_DIR_R, pin)
             else
               clear_bit(GPIO_PORTA_DIR_R, pin)
+              
       case port PORTB:
+        GPIO_PORTB_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTB_DIR_R, pin)
             else
               clear_bit(GPIO_PORTB_DIR_R, pin)
+              
        case port PORTC:
+        GPIO_PORTC_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTC_DIR_R, pin)
             else
               clear_bit(GPIO_PORTC_DIR_R, pin)  
+              
        case port PORTD:
+        GPIO_PORTD_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTD_DIR_R, pin)
             else
               clear_bit(GPIO_PORTD_DIR_R, pin)
+              
         case port PORTE:
+        GPIO_PORTE_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTE_DIR_R, pin)
             else
               clear_bit(GPIO_PORTE_DIR_R, pin)
+              
         case port PORTF:
+        GPIO_PORTF_LOCK_R = 0x4C4F434B;
         if (dir)
           set_bit(GPIO_PORTF_DIR_R, pin)
             else
