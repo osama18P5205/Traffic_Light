83a4de6119fb7b313c90cad2c619cb32bae22605
omarrlotfy <89869341+omarrlotfy@users.noreply.github.com>
2021-11-02 02:13:26 +0200

    Update DIO.c

--- DIO.c
+++ DIO.c
@@ -39,3 +39,31 @@ void DIO.Init (int_8 port, int_8 pin, int_8 dir)
     }
 
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void DIO_WritePort(int_8 port,int_8 value){
+switch(port){
+      case port PORTA:
+        GPIO_PORTA_DATA_R = value;
+        break;
+      case port PORTB:
+        GPIO_PORTB_DATA_R = value;
+        break; 
+      case port PORTC:
+        GPIO_PORTC_DATA_R = value;
+        break;
+      case port PORTD:
+        GPIO_PORTD_DATA_R = value;
+        break;
+      case port PORTE:
+        GPIO_PORTE_DATA_R = value;
+        break;
+      case port PORTF:
+        GPIO_PORTF_DATA_R = value;
+        break;
+    }
+
+
+
+
+}
