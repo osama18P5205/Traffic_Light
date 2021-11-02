0fd9bd3a2836c70d04b2236817fa48b1ad2b4a6c
omarrlotfy <89869341+omarrlotfy@users.noreply.github.com>
2021-11-02 19:37:01 +0200

    Update bitwise_operation.h

--- bitwise_operation.h
+++ bitwise_operation.h
@@ -1,4 +1,4 @@
 #define set_bit(reg,pin) {reg |= (1<<pin);}
 #define clear_bit(reg,pin) {reg &= ~(1<<pin);}
-#define get_bit(reg,pin) {reg & (1<<(pin);}
+#define get_bit(reg,pin) {reg & (1<<(pin)>>pin;}
 #define toggle_bit(reg,pin) {reg ^= (1<<(pin);}
